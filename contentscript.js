f8b5bf5d62ccaee0b4b747003876e3ea80197235
Kevin Zita <bleedgreenandgold@gmail.com>
2015-11-19 19:31:34 -0500

    Stripped away the tags to have the correct information

--- contentscript.js
+++ contentscript.js
@@ -1,11 +1,11 @@
 var site = document.getElementsByClassName("sitestr");
 for (var i = 0; i < site.length; i++){
-    console.log(site[i]);
+    console.log(site[i].textContent);
 }
 
 var rank = document.getElementsByClassName("rank");
 for (var i = 0; i < rank.length; i++){
-    console.log(rank[i]);
+    console.log(rank[i].textContent);
 }
 
 
