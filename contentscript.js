a59b248536d07e75f4fa578cf533545c229ead22
Kyle Robertze <paddatrapper@gmail.com>
2015-11-30 09:55:32 +0200

    refractored the various loops into one

--- contentscript.js
+++ contentscript.js
@@ -1,16 +1,12 @@
 var tags = new Array();
-var rank = document.getElementsByClassName("rank");
-for (var i = 0; i < rank.length; i++){
-    var tag = document.createTextNode(rank[i].textContent + " ");
+var entries = document.getElementsByClassName("athing");
+for (var i = 0; i < entries.length; i++){
+    var rank = entries[i].getElementsByClassName("rank");
+    var tag = document.createTextNode(rank[0].textContent + " ");
     tags.push(tag);
-}
-
-var site = document.getElementsByClassName("sitestr");
-for (var i = 0; i < site.length; i++){
-    tags[i].textContent += site[i].textContent + " ";
-}
-
-var table = document.getElementsByClassName("athing");
-for (var i = 0; i < table.length; i++){
-    table[i].appendChild(tags[i]);
+    var site = entries[i].getElementsByClassName("sitestr");
+    if (site[0] != undefined){
+        tags[i].textContent += site[0].textContent + " ";
+    }
+    entries[i].appendChild(tags[i]);
 }
