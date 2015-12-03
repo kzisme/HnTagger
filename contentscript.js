eab786ca9ad2df6bb42c52e64c5e3d3aac564637
Kevin Zita <bleedgreenandgold@gmail.com>
2015-12-03 01:47:53 -0500

    testing

--- contentscript.js
+++ contentscript.js
@@ -3,9 +3,15 @@ var entries = document.getElementsByClassName("athing");
 
 for (var i = 0; i < entries.length; i++){
     var rank = entries[i].getElementsByClassName("rank");
-    var tag = document.createTextNode(rank[0].textContent + " ");
+    var container = document.createElement("div");
+    var text = document.createTextNode(rank[0].textContent);
     tags.push(tag);
     var site = entries[i].getElementsByClassName("sitestr");
+
+    container.createElement('div');
+    container.appendChild(tags[i]);
+    entries[i].appendChild(container);
+    
     if (site[0] != undefined){
         tags[i].textContent += site[0].textContent + " ";
     }
