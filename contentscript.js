5bfa85d5636458aa192033441d1b40076c35e2c5
Kevin Zita <bleedgreenandgold@gmail.com>
2015-12-03 02:14:23 -0500

    Condensed code - added comments - appended div to athing

--- contentscript.js
+++ contentscript.js
@@ -1,19 +1,27 @@
 var tags = new Array();
 var entries = document.getElementsByClassName("athing");
-
+ 
 for (var i = 0; i < entries.length; i++){
-    var rank = entries[i].getElementsByClassName("rank");
+    // Create a new div container to work with
     var container = document.createElement("div");
-    var text = document.createTextNode(rank[0].textContent);
-    tags.push(tag);
-    var site = entries[i].getElementsByClassName("sitestr");
-
-    container.createElement('div');
-    container.appendChild(tags[i]);
-    entries[i].appendChild(container);
-    
-    if (site[0] != undefined){
-        tags[i].textContent += site[0].textContent + " ";
+ 
+    // Get the elements with class 'rank' from the current entry
+    var rankElements = entries[i].getElementsByClassName("rank");
+    // Create a new text node to hold the rank text
+    var rankText = document.createTextNode(rankElements[0].textContent);
+    // Save the rankText for later I guess?
+    tags.push(rankText);
+ 
+    // Get the elements with class 'sitestr' from the current entry
+    var siteElements = entries[i].getElementsByClassName("sitestr");    
+    // If you got some elements, add the contents of the first one to rankText
+    if (siteElements) {
+        rankText.textContent += siteElements[0].textContent + " ";
     }
-    entries[i].appendChild(tags[i]);
+ 
+    // Put the text node into the container
+    container.appendChild(rankText);
+ 
+    // Put the new div into the current entry
+    entries[i].appendChild(container);
 }
