ec89bbcb5026a8e14de608acc042b868bd51a3a3
Kyle Robertze <paddatrapper@gmail.com>
2016-01-12 08:40:59 +0200

    changed variable naming to be more readable

--- contentscript.js
+++ contentscript.js
@@ -1,7 +1,7 @@
 // This is where ~tag words~ that useres may be interested in are listed
 // Ideally users will be able to add/delete to this in a later version.
 
-var tagLine = [
+var tagKeyWords = [
     "js",
     "javascript",
     "android",
@@ -12,13 +12,13 @@ var tagLine = [
 ];
 
 Array.prototype.slice.call(document.getElementsByClassName("athing")).
-	forEach(function(thing) {
+    forEach(function(thing) {
     // Create elements to be appended (change to valid HTML later)
-    var container = document.createElement("ncolor");
+    var rankSiteContainer = document.createElement("ncolor");
     var titleContainer = document.createElement("tcolor");
 
     // Create an array to hold the rank text
-    var tagTextArr = [thing.getElementsByClassName("rank")[0].textContent];
+    var rankSiteArr = [thing.getElementsByClassName("rank")[0].textContent];
  
     // Get the elements with class 'sitestr' from the current entry
     var siteElement = thing.getElementsByClassName("sitestr");    
@@ -26,23 +26,23 @@ Array.prototype.slice.call(document.getElementsByClassName("athing")).
     // If you got some elements, add the contents of the first one to tagText
     // Check to see if the ~News~ tag should be added
     if (siteElement.length > 0) {
-        tagTextArr.push(siteElement[0].textContent);
+        rankSiteArr.push(siteElement[0].textContent);
     }
-    
-    // Create a text node and put it into the container
-	// Joins an element of the array tagTextArr with a " "
-    container.appendChild(document.createTextNode(tagTextArr.join(" ")));
+
+    // Create a text node and put it into the container of the rank and site
+    // Joins an element of the array rankSiteArr with a " "
+    rankSiteContainer.appendChild(document.createTextNode(rankSiteArr.join(" ")));
 
     // Loops through the links on the page and finds the "Title"
-    // Then compares each title to the array tagLine.
+    // Then compares each title to the array tagKeyWords.
     var titleTd = thing.getElementsByClassName("title")[1]
     var titleText = titleTd.innerHTML;
-    
-    titleContainer.textContent += tagLine.filter(function(tag) {
-    	return titleText.toLowerCase().indexOf(tag) != -1;
+
+    titleContainer.textContent += tagKeyWords.filter(function(tag) {
+        return titleText.toLowerCase().indexOf(tag) != -1;
     }).join(" ");
-    
-    titleTd.appendChild(container);
+
+    titleTd.appendChild(rankSiteContainer);
     if (titleContainer.textContent.length > 0) {
         titleTd.appendChild(titleContainer);
     }
